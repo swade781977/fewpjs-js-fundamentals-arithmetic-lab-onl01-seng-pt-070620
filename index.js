73f939999100b6f30b7c264da4aa6e8bd5d1d576
swade781977 <swade781977@yahoo.com>
2021-02-05 21:32:35 -0500

    commit tree 8d0a49672310ec67978ece2cbe850026d729101b

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 
 // Write your code here
+const num1 = 2
+const num2 = 31
+const num3 = 16
+const num4 = 6
+
+const multiply = num1 * num2
+const random = Math.random()
+const mod = num3 % num4
+const max = Math.max(1,3,6,7,20)
